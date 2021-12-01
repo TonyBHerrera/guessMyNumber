9dbed840c35a362df7df7c47c8731994909d18b7
TonyBHerrera <tonybherrera@yahoo.com>
2021-12-01 14:43:49 -0700

    adding style after winning the game

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'NO number guessed! 🤡';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct!! 👾';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'To High! 🔺';

0744e65185fa021677c27d6f935cae1a26529810
TonyBHerrera <tonybherrera@yahoo.com>
2021-12-01 14:31:52 -0700

    add event listener for check button

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'NO number guessed! 🤡';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct!! 👾';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'To High! 🔺';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose!! 🤡';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'To Low! 🔻';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose! 🤡';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
